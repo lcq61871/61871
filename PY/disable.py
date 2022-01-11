cc01137350dde65d637201ea98e9ba3e493bc736
WinverLemon <787942271@qq.com>
2022-01-11 12:59:09 +0800

    更新 disable.py

--- PY/disable.py
+++ PY/disable.py
@@ -28,7 +28,7 @@ if not ipport:
     ipport = "localhost:5700"
 else:
     ipport = ipport.lstrip("http://").rstrip("/")
-sub_str = os.getenv("RES_SUB", "smiek2121_scripts", "cdle_carry")
+sub_str = os.getenv("RES_SUB", "smiek2121_scripts")
 sub_list = sub_str.split("&")
 res_only = os.getenv("RES_ONLY", True)
 headers = {
